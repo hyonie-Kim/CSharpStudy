f918d9e0687541ba9cd2944d25ddb20c06ba8d14
hyonie <dev.hyonie@gmail.com>
2024-03-26 00:26:39 +0900

    feat: 기본 입출력

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace CSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // input 변수 생성 - 문자열 자료형
+            string input;
+
+            // 출력하는 방법
+            Console.Write("안녕하세요.");
+            Console.Write("C# 공부중");
+            // 마지막에 엔터가 출력됨
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
+            // 사용자 입력 값을 받음
+            // 입력한 값이 그대로 들어옴
+            // 입력이 된 문자열로 바뀜
+            input = Console.ReadLine();
+
+            // 입력 받은 문자열 출력
+            Console.WriteLine("입력 받은 문자열: " + input);
+
+        }
+    }
+}
